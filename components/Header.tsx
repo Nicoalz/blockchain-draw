7338e326711bbfda24db2cc570d114e821ad3c77
Thomas Aubert <90507019+N0rooo@users.noreply.github.com>
2024-11-06 11:31:26 +0100

    :lipstick: style(header): update mobile burger

--- components/Header.tsx
+++ components/Header.tsx
@@ -27,7 +27,7 @@ const Header: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <header className="sticky top-0 flex items-center justify-between w-full px-4 py-6 mb-12 sm:px-10 backdrop-blur-lg bg-custom-dark-blue/70">
+    <header className="sticky top-0 flex items-center justify-between w-full px-4 py-6 mb-12 md:px-10 backdrop-blur-lg bg-custom-dark-blue/70">
       <Link href="/" className="text-2xl font-bold gradient-text">
         Web3 Lottery
       </Link>
@@ -46,8 +46,8 @@ const Header: React.FC = () => {
           transform ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'}
           transition-transform duration-300 ease-in-out
           flex flex-col items-center justify-start pt-20 space-y-6
-          w-3/4 sm:w-1/2 lg:w-1/3 min-h-screen
-          sm:hidden`} // Only show and animate on mobile
+          w-3/4 md:w-1/2 lg:w-1/3 min-h-screen
+          md:hidden`} // Only show and animate on mobile
         >
           {links.map((link, index) => (
             <Link
@@ -64,7 +64,7 @@ const Header: React.FC = () => {
         </nav>
 
         <nav // Desktop menu
-          className={'hidden sm:flex items-center justify-center mr-2'}
+          className={'hidden md:flex items-center justify-center mr-2'}
         >
           <div className="mr-4 space-x-4">
             {links.map((link, index) => {
@@ -91,7 +91,7 @@ const Header: React.FC = () => {
           accountStatus="address"
         />
         <button
-          className="z-50 ml-4 sm:hidden" // Button is only visible on mobile
+          className="z-50 ml-4 md:hidden" // Button is only visible on mobile
           onClick={() => {
             setIsMenuOpen(!isMenuOpen);
           }}
